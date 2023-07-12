76ec122b66794d565aaff90b15bedfc72f575671
Anton Trifonov <toha62@inbox.ru>
2023-07-12 10:45:34 +0300

    initial commit

---
+++ Book.js
@@ -0,0 +1,12 @@
+class Book {
+  constructor(title, description, authors, favorite, fileCover, fileName) {
+    this.title = title;
+    this.description = description;
+    this.authors = authors;
+    this.favorite = favorite;
+    this.fileCover = fileCover;
+    this.fileName = fileName;
+  }
+}
+
+module.exports = Book;
